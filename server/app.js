ddae3b537e6ab69615b455b29d4321811b9fe277
andrew <andrewjthomsen@yahoo.com>
2019-08-10 14:04:24 -0700

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 // import files and packages up here
-
-
+const express = require("express");
+const data = require("./data");
+const morgan = require("morgan");
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.status(200).send("Express Server is working!");
+});
 
+app.get("/data", (req, res) => {
+  res.status(200).send(data);
+});
+app.get("*", (req, res) => {
+  res.status(404).send("Error: page not found!");
+});
 // finally export the express application
 module.exports = app;
